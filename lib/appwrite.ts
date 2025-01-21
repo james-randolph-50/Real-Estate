5c1a3b6d91a9087ccc086d9a59065d52bae9148e
James Randolph <james.randolph2016@gmail.com>
2025-01-21 12:18:42 -0500

    add login/logout functions

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -22,18 +22,42 @@ client
 
     export async function login() {
         try {
-            const redirectUri = Linking.createURL('/');
-
-            const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
-
-            if(!response) throw new Error('Failed to login');
-
-            const browserResult = await openAuthSessionAsync(
-                response.toString(),
-                redirectUri
-            )
+          const redirectUri = Linking.createURL("/");
+      
+          const response = await account.createOAuth2Token(
+            OAuthProvider.Google,
+            redirectUri
+          );
+          if (!response) throw new Error("Create OAuth2 token failed");
+      
+          const browserResult = await openAuthSessionAsync(
+            response.toString(),
+            redirectUri
+          );
+          if (browserResult.type !== "success")
+            throw new Error("Create OAuth2 token failed");
+      
+          const url = new URL(browserResult.url);
+          const secret = url.searchParams.get("secret")?.toString();
+          const userId = url.searchParams.get("userId")?.toString();
+          if (!secret || !userId) throw new Error("Create OAuth2 token failed");
+      
+          const session = await account.createSession(userId, secret);
+          if (!session) throw new Error("Failed to create session");
+      
+          return true;
         } catch (error) {
-            console.error(error);
-            return false;
+          console.error(error);
+          return false;
         }
-    }
+      }
+      
+      export async function logout() {
+        try {
+          const result = await account.deleteSession("current");
+          return result;
+        } catch (error) {
+          console.error(error);
+          return false;
+        }
+      }
