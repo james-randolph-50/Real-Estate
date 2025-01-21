931f98bb90a38823115003d37462d9a6528609d8
James Randolph <james.randolph2016@gmail.com>
2025-01-21 13:00:13 -0500

    login successful

--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -8,14 +8,14 @@ interface User {
     $id: string;
     name: string;
     email: string;
-    avatar: string;
+    avatar?: string;
 }
 
 interface GlobalContextType {
     isLoggedIn: boolean;
     user: User | null;
     loading: boolean;
-    refetch: (newParams?: Record<string, string | number>) => Promise<void>;
+    refetch: (newParams: Record<string, string | number>) => Promise<void>;
 }
 
 const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
