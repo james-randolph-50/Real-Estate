665b6561da07318ffb6fca9e618f3ac437df51c6
James Randolph <james.randolph2016@gmail.com>
2025-01-22 11:54:18 -0500

    fetch properties functionality

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import {Avatars, Client, Account, OAuthProvider, Databases} from "react-native-appwrite"
+import {Avatars, Client, Account, OAuthProvider, Databases, Query} from "react-native-appwrite"
 import * as Linking from 'expo-linking';
 import { openAuthSessionAsync } from "expo-web-browser";
 
@@ -84,4 +84,57 @@ client
             console.error(error);
             return null;
         }
+      }
+
+      export async function getLatestProperties() {
+        try {
+          const result = await databases.listDocuments(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            [Query.orderAsc('$createdAt'), Query.limit(5)]
+          )
+
+          return result.documents;
+        } catch (error) {
+          console.error(error);
+          return [];
+        }
+      }
+
+      export async function getProperties({filter, query, limit}: {
+        filter: string;
+        query: string;
+        limit?: number;
+      }) {
+        try {
+          const buildQuery = [Query.orderDesc('$createdAt')];
+
+          if(filter && filter !== 'All') {
+            buildQuery.push(Query.equal(('type'), filter));
+          }
+
+          if (query)
+            buildQuery.push(
+              Query.or([
+                Query.search("name", query),
+                Query.search("address", query),
+                Query.search("type", query),
+              ])
+            );
+
+            if (limit) buildQuery.push(Query.limit(limit));
+
+            const result = await databases.listDocuments(
+              config.databaseId!,
+              config.propertiesCollectionId!,
+              buildQuery,
+            )
+  
+            return result.documents;
+
+        } catch (error) {
+          console.error(error);
+          return [];
+        }
+
       }
