bdd1d4745e135b5fac1cf5628ae46dbcd6e87451
James Randolph <james.randolph2016@gmail.com>
2025-01-22 07:11:40 -0500

    add tabs

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {Tabs} from "expo-router"
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarStyle: {
+                backgroundColor: 'white',
+                position: 'absolute',
+                borderTopColor: '#0061FF1A',
+                borderTopWidth: 1,
+                minHeight: 70,
+            }
+        }}   
+    >
+      <Text>Tabs</Text>
+    </Tabs>
+  )
+}
+
+export default TabsLayout
