033556a18e840ecd93a0b6547cff68d0a76d7501
James Randolph <james.randolph2016@gmail.com>
2025-01-22 08:08:41 -0500

    render avatar and username dynamically

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,9 +1,12 @@
-import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import icons from "@/constants/icons"
 import images from "@/constants/images"
 import { settings } from "@/constants/data";
+import { useGlobalContext } from '@/lib/global-provider';
+import { logout } from "@/lib/appwrite";
+
 
 interface SettingsItemProp {
   icon: ImageSourcePropType;
@@ -25,8 +28,17 @@ const SettingsItem = ({icon, title, onPress, textStyle, showArrow = true }: Sett
 )
 
 const Profile = () => {
+  const { user, refetch } = useGlobalContext();
 
-  const handleLogout = async () => {};
+  const handleLogout = async () => {
+    const result = await logout();
+    if (result) {
+      Alert.alert("Success", "Logged out successfully");
+      // refetch();
+    } else {
+      Alert.alert("Error", "Failed to logout");
+    }
+  };
 
 
   return (
@@ -43,11 +55,11 @@ const Profile = () => {
 
         <View className='flex-row justify-center flex mt-5'>
           <View className='flex flex-col items-center relative mt-5'>
-            <Image source={images.avatar} className="size-44 relative rounded-full" />
+            <Image source={{uri: user?.avatar}} className="size-44 relative rounded-full" />
             <TouchableOpacity className='absolute bottom-11 right-2'>
               <Image source={icons.edit} className='size-9' />
             </TouchableOpacity>
-            <Text className='text-2xl font-rubik-bold mt-2'>Username</Text>
+            <Text className='text-2xl font-rubik-bold mt-2'>{user?.name}</Text>
           </View>
         </View>
 
@@ -60,6 +72,16 @@ const Profile = () => {
           {settings.slice(2).map((item, index) => (
             <SettingsItem key={index} {...item} />
           ))}
+        </View>
+
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
+          <SettingsItem
+            icon={icons.logout}
+            title="Logout"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
         </View>
 
       </ScrollView>
