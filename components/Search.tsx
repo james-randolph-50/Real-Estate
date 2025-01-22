4211cadaa1b641f764a1c7b8eea07da2d5dc1f68
James Randolph <james.randolph2016@gmail.com>
2025-01-22 08:25:30 -0500

    add search bar style

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Image } from 'react-native'
+import React, { useState } from 'react'
+import { useLocalSearchParams, usePathname } from 'expo-router';
+import icons from '@/constants/icons';
 
 const Search = () => {
+const path = usePathname();
+const params = useLocalSearchParams<{query?: string}>();
+const [search, setSearch] = useState(params.query);
+
+const handleSearch = (text: string) => {
+    setSearch(text);
+}
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
+      <View className="flex-1 flex flex-row items-center justify-start z-50">
+        <Image source={icons.search} className="size-5" />
+        <TextInput
+            value={search}
+            onChangeText={handleSearch}
+            placeholder='Search for anything'
+            className='text-sm font-rubik text-black-300'
+        />
+      </View>
     </View>
   )
 }
